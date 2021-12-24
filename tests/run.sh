70f04c4c50cbf991570bce17872b762185110295
luisnquin <lpaandres2020@gmail.com>
2021-12-24 17:23:33 -0500

    Mod: tests/run.sh(Windows support)

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,5 @@
+echo "You will need to have Python(with pip) and PostgreSQl installed"
+echo ""
 echo "First, create a database, enter the dbname once time are ready: "
 read dbname
 echo "Second, your username: "
@@ -9,20 +11,32 @@ read dbhost
 echo "Port: "
 read dbport
 
-export dbname
-export dbuser
-export dbpassword
-export dbhost
-export dbport
-
-echo "Visit: http://127.0.0.1:8000/docs"
 pip install -r requirements.txt
 
-echo "Linux[1] or Windows[2]: "
+echo "Windows[1], Ubuntu[2]: "
 read option
 
-python3 src/db/migrations.py
-
-
-xdg-open http://127.0.0.1:8000/docs
-uvicorn src.main:app --reload --port=8000
+if [[ "$option" == "1" ]]
+then
+    export dbname
+    export dbuser
+    export dbpassword
+    export dbhost
+    export dbport
+    py src/db/migrations.py
+    echo "Visit: http://127.0.0.1:8000/docs"
+    uvicorn src.main:app --reload --port=8000
+elif [[ "$option" == "2" ]]
+then
+    export dbname
+    export dbuser
+    export dbpassword
+    export dbhost
+    export dbport
+    python3 src/db/migrations.py
+    echo "Visit: http://127.0.0.1:8000/docs or reload the page"
+    xdg-open http://127.0.0.1:8000/docs
+    uvicorn src.main:app --reload --port=8000
+else
+    echo "Bad option"
+fi
